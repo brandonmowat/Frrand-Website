479893e867287cc2149c14072231a303270d7a9b
Brandon <brandonmowat11@gmail.com>
2016-02-27 11:26:11 -0500

    Add dope slideshow in the split-pane section :money_with_wings:

--- js/script.js
+++ js/script.js
@@ -108,3 +108,67 @@ $(document).ready(function() {
 	}
 
 });
+
+// This is script.js
+// You can write JavaScript for your site in here.
+//
+
+// testimonials list
+// list of lists
+
+// Testimonial looper
+var starter;
+
+function Feature() {
+
+	console.log("Initailizing object");
+	var content1 = ['<h2 class="">It\'s about community</h2><p class="">In an age of living online, we find it rare to have personal, physical interactions with one another. Frrand aims to change this by making it easier for people in your community to help each other in meaningful ways such as bringing you the things you need, cost free.</p><p class=""><a href="/help" class="btn">Read some FAQs</a></p>', 'https://static.pexels.com/photos/26274/pexels-photo.jpg'];
+	var content2 = ['<h2 class="">It\'s about the environment</h2><p class="">With so many cars on the roads, sharing vehicles is becoming very popular but isn\'t actually reducing the number of trips. Frrand helps reduce the number of cars and total time on the road by allowing people to help run errands for you, conveniently.</p><p class=""><a href="/help" class="btn">Read some FAQs</a></p>', 'https://static.pexels.com/photos/28505/pexels-photo-28505.jpg'];	
+	this.list = [content1, content2];
+	this.iter = 1;
+	this.len = this.list.length;
+	this.interval = 11000; // in milliseconds
+
+	this.setContent = function(content) {
+		document.getElementById('feature-content').innerHTML = content;
+	}
+	this.setImage = function(url) {
+		document.getElementById('feature-image').style.backgroundImage ="url(\""+url+"\")";
+	}
+};
+
+var iterate = function() {
+	var content = document.getElementById('feature-content');
+	var image = document.getElementById('feature-image');
+	content.style.opacity = 0;
+	image.style.opacity = 0;
+	setTimeout(function() {
+		console.log("Setting new content..." + feature.iter);
+		feature.setContent(feature.list[feature.iter][0]);
+		feature.setImage(feature.list[feature.iter][1]);
+		if (feature.iter === feature.len-1) {
+			feature.iter = 0;
+		}else {
+			feature.iter++;
+		}
+	}, 999);
+	setTimeout(function() {
+		content.style.opacity = 1;
+		image.style.opacity = 1;
+	},1000);
+
+}
+
+Feature.prototype.play = function() {
+	starter = setInterval(iterate, this.interval);
+}
+
+Feature.prototype.pause = function() {
+	starter.clearInterval(this.testimonial);
+}
+
+// Main
+var feature = new Feature();
+feature.play();
+
+
